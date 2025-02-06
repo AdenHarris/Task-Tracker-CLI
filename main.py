84debcb7210e6e53108379000cb7bf7131b1fc0e
Aden Harris <51181390+AdenHarris@users.noreply.github.com>
2025-02-06 16:32:34 +1000

    added json read/write functions

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-
+import json
 
 def add_task(task):
-     print(f"added {task}")
+    print(f"added {task}")
+    x = {
+        "id": 1,
+        "description":"description of task",
+        "status":"todo",
+        "createdAt":"date",
+        "updatedAt":"date"
+         }
+    
+    json_object = json.dumps(x)
+
+    with open('task_data.json', 'w') as outfile:
+        outfile.write(json_object)
+    
+def list():
+    file = open('task_data.json')
+    data = json.load(file)
+    print(data)
 
 # def update_task():
 
@@ -13,6 +30,8 @@ def main():
         user_input = input("#")
         if "add" in str(user_input):
             add_task(user_input[4:])
+        if user_input == "list":
+            list()
 
 
 if __name__ == "__main__":
