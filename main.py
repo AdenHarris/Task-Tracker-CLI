e4dc58f080e85cd32c8f19ab47570b99e1811019
Aden Harris <51181390+AdenHarris@users.noreply.github.com>
2025-02-06 23:42:34 +1000

    Remade tasks as Task Class objects

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
+import datetime
 import json
 
-def add_task(task):
-    print(f"added {task}")
-    x = {
-        "id": 1,
-        "description":"description of task",
-        "status":"todo",
-        "createdAt":"date",
-        "updatedAt":"date"
-         }
-    
-    json_object = json.dumps(x)
+class Task:
+    def __init__(self,id=1,description=None,status="todo",created_at=str(datetime.date.today()),updated_at=str(datetime.date.today())):
+        self.id = id
+        self.description = description
+        self.status = status
+        self.created_at = created_at
+        self.updated_at = updated_at
 
-    with open('task_data.json', 'w') as outfile:
-        outfile.write(json_object)
-    
+    def add_task(self):
+        file = open('task_data.json')
+        data = []
+        data = json.load(file)
+        data.append(self.__dict__)
+
+        json_object = json.dumps(data, indent=0)
+        with open('task_data.json', 'w') as outfile:
+            outfile.write(json_object)
+
+
 def list():
     file = open('task_data.json')
     data = json.load(file)
     print(data)
+
+#reads json file to determine amount of existing tasks
+def load_json_file():
+    file = open('task_data.json')
+    data = []
+    data.append(json.load(file))
+    print(len(data[0]))
+
+    if len(data[0]) == 0:
+        return 1
+    else:
+        return len(data[0])
+
 
 # def update_task():
 
@@ -26,10 +44,15 @@ def list():
 
 
 def main():
+    id_counter = load_json_file()
     while True:
         user_input = input("#")
         if "add" in str(user_input):
-            add_task(user_input[4:])
+            pass
+            task = Task(id=id_counter, description=user_input[4:])
+            #test_task = Task(1,"test","todo","today","today")
+            task.add_task()
+            id_counter += 1
         if user_input == "list":
             list()
 
