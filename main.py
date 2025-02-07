33b1a72d6ab03661c405975f74c86dfaa76a9eb6
Aden Harris <51181390+AdenHarris@users.noreply.github.com>
2025-02-07 21:40:57 +1000

    update task description function

--- main.py
+++ main.py
@@ -18,6 +18,20 @@ class Task:
         json_object = json.dumps(data, indent=0)
         with open('task_data.json', 'w') as outfile:
             outfile.write(json_object)
+    
+def update_task(id,update):
+    file = open('task_data.json')
+    data = []
+    data = json.load(file)
+    for tasks in data:
+        if tasks["id"] == int(id):
+            tasks.update({"description":update})
+            tasks.update({"updated_at":str(datetime.date.today())})
+    print(f"Task {id} updated")
+    json_object = json.dumps(data, indent=0)
+    with open('task_data.json', 'w') as outfile:
+        outfile.write(json_object)
+
 
 
 def list():
@@ -35,10 +49,10 @@ def load_json_file():
     if len(data[0]) == 0:
         return 1
     else:
-        return len(data[0])
+        return len(data[0]) + 1
 
 
-# def update_task():
+
 
 # def delete_task():
 
@@ -48,13 +62,16 @@ def main():
     while True:
         user_input = input("#")
         if "add" in str(user_input):
-            pass
             task = Task(id=id_counter, description=user_input[4:])
             #test_task = Task(1,"test","todo","today","today")
             task.add_task()
+            print(f"Task added successfully (ID:{task.id})")
             id_counter += 1
         if user_input == "list":
             list()
+        
+        if "update" in str(user_input):
+            update_task(user_input[7],user_input[9:])
 
 
 if __name__ == "__main__":
